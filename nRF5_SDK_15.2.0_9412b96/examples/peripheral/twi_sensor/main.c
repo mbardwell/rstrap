dec4b40c0627d01e98ef3c37c6fb2fbf7db94441
Mike <bardwell@ualberta.ca>
2019-07-12 13:21:38 -0600

    wip - started adapting twi example to bma2x2 api

--- nRF5_SDK_15.2.0_9412b96/examples/peripheral/twi_sensor/main.c
+++ nRF5_SDK_15.2.0_9412b96/examples/peripheral/twi_sensor/main.c
@@ -62,6 +62,13 @@
 /* TWI instance ID. */
 #define TWI_INSTANCE_ID     0
 
+/* I2C defines */
+#define	I2C_BUFFER_LEN      8
+#define BMA2x2_BUS_READ_WRITE_ARRAY_INDEX	1
+
+#define ADDR_LSB            22
+#define SDA_PIN             20
+#define SCL_PIN             19
 /* Common addresses definition for temperature sensor. */
 #define LM75B_ADDR          (0x90U >> 1)
 
@@ -91,14 +98,14 @@ void LM75B_set_mode(void)
 
     /* Writing to LM75B_REG_CONF "0" set temperature sensor in NORMAL mode. */
     uint8_t reg[2] = {LM75B_REG_CONF, NORMAL_MODE};
-    err_code = nrf_drv_twi_tx(&m_twi, LM75B_ADDR, reg, sizeof(reg), false);
+    err_code = nrf_drv_twi_tx(&m_twi, BMA2x2_I2C_ADDR1, reg, sizeof(reg), false);
     APP_ERROR_CHECK(err_code);
     while (m_xfer_done == false);
 
     /* Writing to pointer byte. */
     reg[0] = LM75B_REG_TEMP;
     m_xfer_done = false;
-    err_code = nrf_drv_twi_tx(&m_twi, LM75B_ADDR, reg, 1, false);
+    err_code = nrf_drv_twi_tx(&m_twi, BMA2x2_I2C_ADDR1, reg, 1, false);
     APP_ERROR_CHECK(err_code);
     while (m_xfer_done == false);
 }
@@ -140,8 +147,8 @@ void twi_init (void)
     ret_code_t err_code;
 
     const nrf_drv_twi_config_t twi_lm75b_config = {
-       .scl                = ARDUINO_SCL_PIN,
-       .sda                = ARDUINO_SDA_PIN,
+       .scl                = SCL_PIN,
+       .sda                = SDA_PIN,
        .frequency          = NRF_DRV_TWI_FREQ_100K,
        .interrupt_priority = APP_IRQ_PRIORITY_HIGH,
        .clear_bus_init     = false
@@ -151,18 +158,92 @@ void twi_init (void)
     APP_ERROR_CHECK(err_code);
 
     nrf_drv_twi_enable(&m_twi);
+
+    // config output pin to zero
+    nrf_gpio_cfg_output(ADDR_LSB);
+    nrf_gpio_pin_clear(ADDR_LSB);
 }
 
-/**
- * @brief Function for reading data from temperature sensor.
- */
-static void read_sensor_data()
-{
-    m_xfer_done = false;
+// /**
+//  * @brief Function for reading data from temperature sensor.
+//  */
+// static void read_sensor_data()
+// {
+//     m_xfer_done = false;
 
-    /* Read 1 byte from the specified address - skip 3 bits dedicated for fractional part of temperature. */
-    ret_code_t err_code = nrf_drv_twi_rx(&m_twi, LM75B_ADDR, &m_sample, sizeof(m_sample));
-    APP_ERROR_CHECK(err_code);
+//     /* Read 1 byte from the specified address - skip 3 bits dedicated for fractional part of temperature. */
+//     ret_code_t err_code = nrf_drv_twi_rx(&m_twi, BMA2x2_I2C_ADDR1, &m_sample, sizeof(m_sample));
+//     APP_ERROR_CHECK(err_code);
+// }
+
+s32 init_bma(struct bma2x2_t bma2x2)
+{
+    s32 com_rslt = ERROR;
+
+    com_rslt = bma2x2_init(&bma2x2);
+    APP_ERROR_CHECK(com_rslt);
+    com_rslt += bma2x2_set_power_mode(BMA2x2_MODE_NORMAL);
+
+    u8 bw_value_u8 = BMA2x2_INIT_VALUE;
+    bw_value_u8 = 0x08;/* set bandwidth of 7.81Hz*/
+	com_rslt += bma2x2_set_bw(bw_value_u8);
+
+    u8 banwid = BMA2x2_INIT_VALUE;
+	/* This API used to read back the written value of bandwidth*/
+	com_rslt += bma2x2_get_bw(&banwid);
+
+    return com_rslt;
+}
+
+s8 bma_i2c_write(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt)
+{
+	s32 iError = BMA2x2_INIT_VALUE;
+	u8 array[I2C_BUFFER_LEN];
+	u8 stringpos = BMA2x2_INIT_VALUE;
+
+    array[BMA2x2_INIT_VALUE] = reg_addr;
+	for (stringpos = BMA2x2_INIT_VALUE; stringpos < cnt; stringpos++) {
+		array[stringpos + BMA2x2_BUS_READ_WRITE_ARRAY_INDEX] =
+		*(reg_data + stringpos);
+	}
+
+    m_xfer_done = false;
+    iError = nrf_drv_twi_tx(&m_twi, dev_addr, array, sizeof(array), false);
+    NRF_LOG_INFO("error: %d, array: %d", (ret_code_t) iError, array[0]);
+    NRF_LOG_FLUSH();
+    APP_ERROR_CHECK((ret_code_t) iError);
+    while (m_xfer_done == false);
+
+
+    return (s8) iError;
+}
+
+s8 bma_i2c_read(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt)
+{
+	s32 iError = BMA2x2_INIT_VALUE;
+	u8 array[I2C_BUFFER_LEN] = {BMA2x2_INIT_VALUE};
+	u8 stringpos = BMA2x2_INIT_VALUE;
+
+    bma_i2c_write(dev_addr, reg_addr, NULL, 0); // write control byte
+
+    nrf_delay_us(100);
+
+	array[BMA2x2_INIT_VALUE] = reg_addr;
+
+    m_xfer_done = false;
+    iError = nrf_drv_twi_rx(&m_twi, BMA2x2_I2C_ADDR1, array, cnt);
+    APP_ERROR_CHECK((ret_code_t) iError);
+
+    for (stringpos = BMA2x2_INIT_VALUE; stringpos < cnt; stringpos++) {
+		*(reg_data + stringpos) = array[stringpos];
+    }
+
+	return (s8)iError;
+}
+
+void i2c_delay(u32 millis_time)
+{
+    nrf_delay_ms(millis_time);
 }
 
 /**
@@ -173,10 +254,23 @@ int main(void)
     APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
     NRF_LOG_DEFAULT_BACKENDS_INIT();
 
-    NRF_LOG_INFO("\r\nTWI sensor example started.");
-    NRF_LOG_FLUSH();
+    NRF_LOG_INFO("\r\n starting twi example");
+
     twi_init();
     LM75B_set_mode();
+    NRF_LOG_INFO("\r\n initialised twi");
+
+    struct bma2x2_t bma2x2;
+    bma2x2.bus_write = &bma_i2c_write;
+    bma2x2.bus_read = &bma_i2c_read;
+    bma2x2.dev_addr = BMA2x2_I2C_ADDR1;
+    bma2x2.delay_msec = &i2c_delay;
+    init_bma(bma2x2);
+    NRF_LOG_INFO("\r\n initialised BMA2x2");
+
+    s32 com_rslt = ERROR;
+    /* bma2x2acc_data structure used to read accel xyz data*/
+	struct bma2x2_accel_data sample_xyz;
 
     while (true)
     {
@@ -187,7 +281,12 @@ int main(void)
             __WFE();
         }while (m_xfer_done == false);
 
-        read_sensor_data();
+        /* accessing the bma2x2acc_data parameter by using sample_xyz*/
+        /* Read the accel XYZ data*/
+        com_rslt += bma2x2_read_accel_xyz(&sample_xyz);
+        APP_ERROR_CHECK((ret_code_t) com_rslt);
+
+        NRF_LOG_INFO("X: %d, Y: %d, Z: %d", sample_xyz.x, sample_xyz.y, sample_xyz.z);
         NRF_LOG_FLUSH();
     }
 }
