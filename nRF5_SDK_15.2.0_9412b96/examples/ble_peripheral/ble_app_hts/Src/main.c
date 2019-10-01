6eaae0f7df4524be38af1e98fe77567d21ab8b52
Mike <bardwell@ualberta.ca>
2019-10-01 00:25:55 -0600

    Fixed itoa warning. To be checked. Moved defines to config.h. Removed comments from makefile

--- nRF5_SDK_15.2.0_9412b96/examples/ble_peripheral/ble_app_hts/Src/main.c
+++ nRF5_SDK_15.2.0_9412b96/examples/ble_peripheral/ble_app_hts/Src/main.c
@@ -90,58 +90,6 @@
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
 
-#define DEVICE_NAME "Nordic_HTS"                /**< Name of device. Will be included in the advertising data. */
-#define MANUFACTURER_NAME "NordicSemiconductor" /**< Manufacturer. Will be passed to Device Information Service. */
-#define MODEL_NUM "NS-HTS-EXAMPLE"              /**< Model number. Will be passed to Device Information Service. */
-#define MANUFACTURER_ID 0x1122334455            /**< Manufacturer ID, part of System ID. Will be passed to Device Information Service. */
-#define ORG_UNIQUE_ID 0x667788                  /**< Organizational Unique ID, part of System ID. Will be passed to Device Information Service. */
-
-#define APP_BLE_OBSERVER_PRIO 3 /**< Application's BLE observer priority. You shouldn't need to modify this value. */
-#define APP_BLE_CONN_CFG_TAG 1  /**< A tag identifying the SoftDevice BLE configuration. */
-
-#define APP_ADV_INTERVAL 40 /**< The advertising interval (in units of 0.625 ms. This value corresponds to 25 ms). */
-
-#define APP_ADV_DURATION 18000 /**< The advertising duration (180 seconds) in units of 10 milliseconds. */
-
-#define BATTERY_LEVEL_MEAS_INTERVAL APP_TIMER_TICKS(2000) /**< Battery level measurement interval (ticks). */
-#define MIN_BATTERY_LEVEL 81                              /**< Minimum battery level as returned by the simulated measurement function. */
-#define MAX_BATTERY_LEVEL 100                             /**< Maximum battery level as returned by the simulated measurement function. */
-#define BATTERY_LEVEL_INCREMENT 1                         /**< Value by which the battery level is incremented/decremented for each call to the simulated measurement function. */
-
-#define TEMP_TYPE_AS_CHARACTERISTIC 0 /**< Determines if temperature type is given as characteristic (1) or as a field of measurement (0). */
-
-#define MIN_CELCIUS_DEGREES 3688     /**< Minimum temperature in celcius for use in the simulated measurement function (multiplied by 100 to avoid floating point arithmetic). */
-#define MAX_CELCIUS_DEGRESS 3972     /**< Maximum temperature in celcius for use in the simulated measurement function (multiplied by 100 to avoid floating point arithmetic). */
-#define CELCIUS_DEGREES_INCREMENT 36 /**< Value by which temperature is incremented/decremented for each call to the simulated measurement function (multiplied by 100 to avoid floating point arithmetic). */
-
-#define MIN_CONN_INTERVAL MSEC_TO_UNITS(50, UNIT_1_25_MS)  /**< Minimum acceptable connection interval (0.5 seconds) */
-#define MAX_CONN_INTERVAL MSEC_TO_UNITS(100, UNIT_1_25_MS) /**< Maximum acceptable connection interval (1 second). */
-#define SLAVE_LATENCY 0                                     /**< Slave latency. */
-#define CONN_SUP_TIMEOUT MSEC_TO_UNITS(4000, UNIT_10_MS)    /**< Connection supervisory timeout (4 seconds). */
-
-#define FIRST_CONN_PARAMS_UPDATE_DELAY APP_TIMER_TICKS(5000) /**< Time from initiating event (connect or start of indication) to first time sd_ble_gap_conn_param_update is called (5 seconds). */
-#define NEXT_CONN_PARAMS_UPDATE_DELAY APP_TIMER_TICKS(30000) /**< Time between each call to sd_ble_gap_conn_param_update after the first call (30 seconds). */
-#define MAX_CONN_PARAMS_UPDATE_COUNT 3                       /**< Number of attempts before giving up the connection parameter negotiation. */
-
-#define SEC_PARAM_BOND 0                               /**< Perform bonding. */
-#define SEC_PARAM_MITM 0                               /**< Man In The Middle protection not required. */
-#define SEC_PARAM_LESC 0                               /**< LE Secure Connections not enabled. */
-#define SEC_PARAM_KEYPRESS 0                           /**< Keypress notifications not enabled. */
-#define SEC_PARAM_IO_CAPABILITIES BLE_GAP_IO_CAPS_NONE /**< No I/O capabilities. */
-#define SEC_PARAM_OOB 0                                /**< Out Of Band data not available. */
-#define SEC_PARAM_MIN_KEY_SIZE 7                       /**< Minimum encryption key size. */
-#define SEC_PARAM_MAX_KEY_SIZE 16                      /**< Maximum encryption key size. */
-
-#define DEAD_BEEF 0xDEADBEEF /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
-
-#define ADC_REF_VOLTAGE_IN_MILLIVOLTS 600  //!< Reference voltage (in milli volts) used by ADC while doing conversion.
-#define DIODE_FWD_VOLT_DROP_MILLIVOLTS 270 //!< Typical forward voltage drop of the diode (Part no: SD103ATW-7-F) that is connected in series with the voltage supply. This is the voltage drop when the forward current is 1mA. Source: Data sheet of 'SURFACE MOUNT SCHOTTKY BARRIER DIODE ARRAY' available at www.diodes.com.
-#define ADC_RES_10BIT 1024                 //!< Maximum digital value for 10-bit ADC conversion.
-#define ADC_PRE_SCALING_COMPENSATION 6     //!< The ADC is configured to use VDD with 1/3 prescaling as input. And hence the result of conversion is to be multiplied by 3 to get the actual value of the battery voltage.
-#define LOWEST_ALLOWABLE_BATTERY_VOLTAGE 2120
-#define NOMINAL_FRESH_BATTERY_VOLTAGE 3000
-
-
 APP_TIMER_DEF(m_hts_timer_id);      // Health thermometer service timer
 APP_TIMER_DEF(m_tension_timer_id);  // Tension timer
 BLE_NUS_DEF(m_nus, 1);              // Nordic UART Service structure
@@ -251,7 +199,7 @@ static void TensionLevelUpdate(void)
         if (Hx711SampleConvert(&tension_level) == NRFX_SUCCESS)
         {
             len = (uint16_t) max_n_digits;
-            itoa(tension_level, tension, 10);
+            __itoa(tension_level, (char*) tension, 10);
             NRF_LOG_INFO("Sending tension measurement: %d of length: %d", tension_level, len);
 
             err_code = ble_nus_data_send(&m_nus, tension, &len, m_conn_handle);
